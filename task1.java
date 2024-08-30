bc5a1119308906410aeee91a13691fc1a1f1ebe5
Aqib Ahmed <f23ari35@aror.edu.pk>
2024-08-30 19:42:14 +0500

    task1.java

---
+++ task1.java
@@ -0,0 +1,17 @@
+public class task1 {
+    public static void main(String[] args) {
+        
+        int[] arr={5,15,25,35,45,55};
+        
+        System.out.println("Exercise 1:");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+        
+        System.out.println("\n\nExercise 2:");
+        for(int i=0; i<arr.length; i++){
+            System.out.println("Element at index "+i+" : "+arr[i]);
+        }
+    }
+}
