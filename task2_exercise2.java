97db8ba5bd6d256e6b40643090c7c00adbc9ca41
Aqib Ahmed <f23ari35@aror.edu.pk>
2024-08-30 20:17:47 +0500

    task 02 exercises 2 and 3 modified

--- task2_exercise2.java
+++ task2_exercise2.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author EURO COMPUTERS
- */
 public class task2_exercise2 {
     public static void main(String[] args) {
         int[] arr = new int[6];
--- task2_exercise3.java
+++ task2_exercise3.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author EURO COMPUTERS
- */
 public class task2_exercise3 {
     public static void main(String[] args) {
         
