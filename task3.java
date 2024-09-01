599cd0c43fa906173233c725c995272da5003198
Aqib Ahmed <f23ari35@aror.edu.pk>
2024-09-01 13:03:31 +0500

    task 03

--- task3.java
+++ task3.java
@@ -1,26 +1,21 @@
 public class task3 {
     public static void main(String[] args) {
         
-        int[] arr = new int[6];
-        arr[0]=18;
-        arr[1]=16;
-        arr[2]=24;
-        arr[3]=32;
-        arr[4]=40;
-        
-        System.out.println("Before insertion");
+        int[] arr = {8,16,24,32,40};
+
+        System.out.println("Before Deletion");
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");   
         }
         
         int p=2;
-        for(int i=arr.length-1; i>p; i--){
-               arr[i]=arr[i-1];
+        for(int i=p; i<arr.length-1;  i++){
+               arr[i]=arr[i+1];
         }
+        arr[arr.length-1]=0;
         
-        int insert=250;
-        arr[0]=insert;
-        System.out.println("\n\nAfter insertion");
+        
+        System.out.println("\n\nAfter deletion");
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");   
         } 
